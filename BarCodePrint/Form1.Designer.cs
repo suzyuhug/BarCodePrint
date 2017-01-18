58e94d3e6d8d1dcc5e2805db3500634f0f38879e
suzyuhug <Yufei.huang@flextronics.com>
2017-01-18 09:30:01 +0800

    功能完成

--- BarCodePrint/Form1.Designer.cs
+++ BarCodePrint/Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtpn = new System.Windows.Forms.TextBox();
+            this.txtpo = new System.Windows.Forms.TextBox();
+            this.butprt = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PN:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PO:";
+            // 
+            // txtpn
+            // 
+            this.txtpn.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtpn.Location = new System.Drawing.Point(163, 55);
+            this.txtpn.MaxLength = 16;
+            this.txtpn.Name = "txtpn";
+            this.txtpn.Size = new System.Drawing.Size(178, 20);
+            this.txtpn.TabIndex = 2;
+            // 
+            // txtpo
+            // 
+            this.txtpo.Location = new System.Drawing.Point(163, 108);
+            this.txtpo.MaxLength = 6;
+            this.txtpo.Name = "txtpo";
+            this.txtpo.Size = new System.Drawing.Size(178, 20);
+            this.txtpo.TabIndex = 3;
+            // 
+            // butprt
+            // 
+            this.butprt.Location = new System.Drawing.Point(298, 221);
+            this.butprt.Name = "butprt";
+            this.butprt.Size = new System.Drawing.Size(84, 37);
+            this.butprt.TabIndex = 4;
+            this.butprt.Text = "Print";
+            this.butprt.UseVisualStyleBackColor = true;
+            this.butprt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(451, 313);
+            this.Controls.Add(this.butprt);
+            this.Controls.Add(this.txtpo);
+            this.Controls.Add(this.txtpn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MC-BarCode Print";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpn;
+        private System.Windows.Forms.TextBox txtpo;
+        private System.Windows.Forms.Button butprt;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
     }
 }
 
--- BarCodePrint/Form1.cs
+++ BarCodePrint/Form1.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +18,70 @@ namespace BarCodePrint
         public Form1()
         {
             InitializeComponent();
+           
+        }   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtpn.Text!="" ||txtpo.Text!="")
+            {
+                this.printDocument1.DefaultPageSettings.PaperSize = new PaperSize("Custum", 600, 800);           
+                this.printDocument1.PrintPage += new PrintPageEventHandler(this.MyPrintDocument_PrintPage);
+                PrintController printController = new StandardPrintController();
+                printDocument1.PrintController = printController;
+                this.printDocument1.Print();
+                this.printDocument1.PrintPage -= new PrintPageEventHandler(this.MyPrintDocument_PrintPage);
+            }
+            else
+            {               
+                MessageBox.Show("请输入PN或PO！", "系统消息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+       
+
+
+                    }        private void MyPrintDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)        {           
+            e.Graphics.TranslateTransform(200, 30);
+            e.Graphics.RotateTransform(90.0F);
+            if (txtpn.Text!="")
+            {
+                e.Graphics.DrawString($"PN:{txtpn.Text}", new Font(new FontFamily("黑体"), 15), System.Drawing.Brushes.Black, 0, 0);
+                e.Graphics.DrawString($"*{txtpn.Text}*", fn, System.Drawing.Brushes.Black, 0, 30);
+            }
+            if (txtpo.Text!="")
+            {
+                e.Graphics.DrawString($"PO:{txtpo.Text}", new Font(new FontFamily("黑体"), 15), System.Drawing.Brushes.Black, 0, 80);
+                e.Graphics.DrawString($"*{txtpo.Text}*", fn, System.Drawing.Brushes.Black, 0, 110);
+            
+            }
+                    
+        }
+        System.Drawing.Font fn;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string fontspath = $"{Application.StartupPath.ToString()}\\Fonts\\FRE3OF9X.TTF";
+            if (File.Exists(fontspath))
+            {
+                PrivateFontCollection privateFonts = new PrivateFontCollection();
+                privateFonts.AddFontFile(fontspath);
+                fn = new Font(privateFonts.Families[0], 28);
+
+
+            }
+            else
+            {
+                MessageBox.Show("条码字体不存在！", "系统消息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+          
+     
+
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+          
         }
     }
 }
